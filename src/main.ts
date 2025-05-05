db10e7480fe6373ed74632e168a3a89d03966b73
João Pedro Carlos da Silva <joaopcarlosdasilva@gmail.com>
2025-05-05 19:40:49 -0300

    fix(erro-cors): corrige erro de cors para frontend comunicar

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,21 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
- );
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
