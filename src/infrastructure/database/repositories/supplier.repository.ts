c4e039ac169518743729edb815e2e7f3b8764141
João Pedro Carlos da Silva <joaopcarlosdasilva@gmail.com>
2025-06-03 21:31:57 -0300

    ajustes getall fornecedores

--- src/infrastructure/database/repositories/supplier.repository.ts
+++ src/infrastructure/database/repositories/supplier.repository.ts
@@ -7,32 +7,45 @@ import { SupplierEntity } from 'src/domain/entities';
 @Injectable()
 export class SupplierRepository extends BaseRepository<SupplierEntity> {
   constructor(
-    @InjectRepository(SupplierEntity) private _repository: Repository<SupplierEntity>,
+    @InjectRepository(SupplierEntity)
+    private _repository: Repository<SupplierEntity>,
   ) {
     super(_repository);
   }
 
-async findWithRelations(): Promise<SupplierEntity[]> {
-  return this._repository
-    .createQueryBuilder('supplier')
-    .select([
-      'supplier.id',
-      'supplier.name',
-      'supplier.email',
-      'supplier.address',
-      'supplier.created_at',
-      'supplier.edited_at',
-      'supplier.archived_at',  
-      'supplier.archived', 
-    ])
-    .orderBy('supplier.name', 'ASC')
-    .getMany();
-}
+  async findWithRelations(): Promise<SupplierEntity[]> {
+    return this._repository
+      .createQueryBuilder('supplier')
+      .leftJoinAndSelect('supplier.createdBy', 'creator')
+      .leftJoinAndSelect('supplier.editedBy', 'editor')
+      .leftJoinAndSelect('supplier.archivedBy', 'archiver')
+      .select([
+        'supplier.id',
+        'supplier.name',
+        'supplier.email',
+        'supplier.address',
+        'supplier.created_at',
+        'supplier.edited_at',
+        'supplier.archived_at',
+        'supplier.archived',
+        'creator.id',
+        'creator.name',
+        'creator.last_name',
+        'editor.id',
+        'editor.name',
+        'editor.last_name',
+        'archiver.id',
+        'archiver.name',
+        'archiver.last_name',
+      ])
+      .orderBy('supplier.name', 'ASC')
+      .getMany();
+  }
 
   async getAllForSelect(): Promise<Partial<SupplierEntity[]>> {
     return this._repository
-      .createQueryBuilder('supplier')	
-      .select(['supplier.id', 'supplier.name', 'supplier.last_name']) 
+      .createQueryBuilder('supplier')
+      .select(['supplier.id', 'supplier.name', 'supplier.last_name'])
       .where('supplier.archived = false')
       .getMany();
   }
